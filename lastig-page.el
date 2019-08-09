02b34572d461523fbd86935fb7a8d85f7a6b86d8
mbunel <mattiabunel@hotmail.fr>
2019-08-10 01:58:33 +0200

    page perso v0

---
+++ lastig-page.el
@@ -0,0 +1,17 @@
+(package-initialize)
+(require 'org)
+
+
+(defun pd-html-publish-to-html (plist filename pub-dir)
+  (org-publish-org-to 'pd-html filename ".html" plist pub-dir))
+
+(setq org-publish-project-alist
+      '(("lastig_page"
+	 :base-directory "./src"
+	 :publishing-directory "./"
+	 :publishing-function org-html-publish-to-html
+	 :auto-sitemap nil)))
+
+(org-mode)
+(org-publish-project "lastig_page")
+
---
+++ makefile
@@ -0,0 +1,4 @@
+all:
+	emacs --batch -l ./lastig-page.el
+clean:
+	rm ./*html
